fbd8b930c6e5ec900f53a5b53de2a1851886fd28
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-18 10:07:21 -0500

    Agrega los iconos sociales

---
+++ src/assets/Options.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 24 24" width="30px" height="20px"><g fill="currentColor"><circle cx="5" cy="12" r="2"></circle><circle cx="12" cy="12" r="2"></circle><circle cx="19" cy="12" r="2"></circle></g></svg>
+</template>
