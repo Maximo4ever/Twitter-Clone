6307e18f95982ffab02d3acad165bbf5aaae98b1
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-17 23:26:47 -0500

    Añade la configuracion inicial del proyecto

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+<h1>Hello World</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: #15202B;
+  color: #fff;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+</style>
