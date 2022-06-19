b4ef33b49410bb794b128487c1b6351cd3c09f2f
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-19 09:32:15 -0500

    Agrega el aside de la app

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,10 +1,69 @@
+<script setup>
+import SearchIcon from "../assets/SearchIcon.vue";
+import AsideUser from "./AsideUser.vue";
+</script>
 <template>
   <aside class="aside">
-    <h2>Aside</h2>
+    <div class="aside__content">
+      <form class="form">
+        <SearchIcon width="20px" height="20px" />
+        <input class="form__input" type="text" placeholder="Search Twitter">
+      </form>
+      <section class="section">
+        <h3>What’s happening</h3>
+      </section>
+      <section class="section">
+        <h3>Who to follow</h3>
+        <div class="userWhoToFollow">
+          <AsideUser />
+          <AsideUser />
+          <AsideUser />
+          <AsideUser />
+          <AsideUser />
+        </div>
+      </section>
+    </div>
   </aside>
 </template>
 <style>
 .aside {
   width: 350px;
+  height: 100%;
+  margin-left: 30px;
+  /* background-color: #252E38; */
+}
+.aside__content {
+  width: 350px;
+  position: fixed;
+  top: 0;
+}
+.form {
+  position: relative;
+  padding-top: 12px;
+}
+.form__input {
+  width: 100%;
+  font-size: 15px;
+  padding: 14px 10px 14px 55px;
+  background-color: #273340;
+  color: #fff;
+  border-radius: 30px;
+  border: none;
+  outline: none;
+}
+.form__input::placeholder {
+  color: #8B98A5;
+}
+.section {
+  background-color: #1E2732;
+  padding-block: 12px;
+  border-radius: 16px;
+  margin-top: 16px;
+}
+.section h3 {
+  padding-inline: 16px;
+}
+.userWhoToFollow {
+  padding-top: 10px;
 }
 </style>
---
+++ src/components/AsideUser.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="user">
+    <img class="user__img" src="https://thispersondoesnotexist.com/image" alt="random">
+    <div>
+      <div>
+        <h6 class="user__name">Jhon Doe</h6>
+        <span class="user__at">@elonmusk</span>
+      </div>
+      <button class="user__btn">Follow</button>
+    </div>
+  </div>
+</template>
+<style>
+.user {
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+  padding-block: 11px;
+  padding-inline: 16px;
+  cursor: pointer;
+}
+.user:hover {
+  background-color: #252E38;
+}
+.user__img {
+  height: 52px;
+  width: 52px;
+  border-radius: 50%;
+}
+.user > div {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+.user h6, .user span {
+  font-size: 15px;
+}
+.user__name:hover {
+  text-decoration: underline;
+}
+.user span {
+  color: #8b98a5;
+}
+.user__btn {
+  font-size: 14px;
+  height: 32px;
+  font-weight: 600;
+  padding-inline: 19px;
+  border: none;
+  border-radius: 16px;
+  cursor: pointer;
+}
+.user__btn:hover {
+  background-color: #D7DBDC;
+}
+</style>
