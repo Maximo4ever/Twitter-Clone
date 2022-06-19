2f55f02b923c5309d4250d74beacccffccf1776b
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-19 09:32:59 -0500

    Fija el nav del main arriba

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,10 +8,13 @@
 .nav {
   height: 53px;
   position: sticky;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
   font-size: 20px;
   padding-inline: 16px;
+  background-color: #15202bcc;
+  backdrop-filter: blur(30px);
 }
 </style>
